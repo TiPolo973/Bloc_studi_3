b9ad54f5129bd24c6a55c55143453cc5e29c1480
Paul Jacotin <Pauljacotin@outlook.com>
2024-04-19 08:42:30 -0300

    Create first page with abstractcontroller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController {
+    
+
+    #[Route('/')]
+    public function number() {
+        $number = rand(0,100);
+        return $this->render('base.html.twig',['chat' => $number ]);
+    }
+
+
+}
