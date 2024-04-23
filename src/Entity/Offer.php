2c6b959233f20d5836132e656c4a86d60bc1c68d
Paul Jacotin <Pauljacotin@outlook.com>
2024-04-23 16:05:32 -0300

    Create entity Offer

---
+++ src/Entity/Offer.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OfferRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OfferRepository::class)]
+class Offer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $location = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_time = null;
+
+    #[ORM\Column]
+    private ?int $plan = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getDateTime(): ?\DateTimeInterface
+    {
+        return $this->date_time;
+    }
+
+    public function setDateTime(\DateTimeInterface $date_time): static
+    {
+        $this->date_time = $date_time;
+
+        return $this;
+    }
+
+    public function getPlan(): ?int
+    {
+        return $this->plan;
+    }
+
+    public function setPlan(int $plan): static
+    {
+        $this->plan = $plan;
+
+        return $this;
+    }
+}
